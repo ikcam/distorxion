d08ddb6ad3daad8252b8149ffce90fb656d093d6
Irving Kcam <xploit29@gmail.com>
2012-12-07 22:37:34 -0500

    Player javascript

--- javascript/sandbox.jquery.js
+++ javascript/sandbox.jquery.js
@@ -16,4 +16,36 @@ jQuery(document).ready(function($){
 			collapsible: true
 		});
 	});
+
+	var volume = 0.7;
+	
+	$('#audio')[0].volume=volume;
+
+	$('#bt-play').click(function(){
+		$('#audio')[0].play();
+	});
+
+	$('#bt-pause').click(function(){
+		$('#audio')[0].stop();
+	});
+
+	$('#bt-voldown').click(function(){
+		$('#audio')[0].volume-=0.1;
+	});
+
+	$('#bt-volup').click(function(){
+		$('#audio')[0].volume+=0.1;
+	});
+
+	$('#bt-mute').click(function(){
+		if( $('#audio')[0].volume === 0 ){
+			$('#audio')[0].volume = volume;
+			$(this).text('Mute');
+		} else {
+			volume = $('#audio')[0].volume;
+			$('#audio')[0].volume=0;
+			$(this).text('Unmute');
+		}
+	})
+
 });
