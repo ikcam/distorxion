e2fc4ccc9b2926a69fd47cf4bee3b4ff175e3648
Irving Kcam <xploit29@gmail.com>
2012-12-11 21:18:06 -0500

    mp3

--- javascript/sandbox.jquery.js
+++ javascript/sandbox.jquery.js
@@ -17,7 +17,7 @@ jQuery(document).ready(function($){
 		});
 	});
 
-  var AUDIO_FILE = 'http://distorxionradio.com:8000/;stream.mp4',
+  var AUDIO_FILE = 'http://distorxionradio.com:8000/;stream.mp3',
     waveform = $('#audio')[0],
     ctx = waveform.getContext('2d'),
     dancer, kick;
@@ -41,7 +41,7 @@ jQuery(document).ready(function($){
   }).on();
 
   dancer
-    .load({ src: AUDIO_FILE, codecs: [ 'ogg', 'mp3', 'mp4' ]})
+    .load({ src: AUDIO_FILE, codecs: [ 'ogg', 'mp3' ]})
     .waveform( waveform, { strokeStyle: '#666', strokeWidth: 2 });
 
   Dancer.isSupported() || loaded();
